aff49d237c40be6f96d406bd3712e0f313cc13c9
Jake Pittis <jakepittis@gmail.com>
2018-01-21 18:07:06 -0500

    remove http module

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,27 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import Orchestrator (startOrchestrator)
-import Http
+import Orchestrator (startOrchestrator, OrchReader, runCommand)
 
-import Web.Scotty.Trans (scottyT)
+import Web.Scotty.Trans
+import Data.Text.Lazy
 import Control.Monad.Trans.Reader (runReaderT)
+
+import Control.Monad.Trans.Class (lift)
+
+routes :: ScottyT Text OrchReader ()
+routes = do
+  get    "/proxies"       respondToCommand
+  post   "/proxies"       respondToCommand
+  get    "/proxies/:name" respondToCommand
+  delete "/proxies/:name" respondToCommand
+  put    "/proxies/:name" respondToCommand
+  where
+    respondToCommand = do
+      command <- jsonData
+      response <- lift $ runCommand command
+      json response
 
 main :: IO ()
 main = do
