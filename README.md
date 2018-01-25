43d79ef52eb69ba1defbdc1c9074fa1f27b7063e
Jake Pittis <jakepittis@gmail.com>
2018-01-25 16:42:19 -0500

    update README with some examples

--- README.md
+++ README.md
@@ -9,3 +9,38 @@ A simple but effective failure testing proxy.
 - Fuzzy testing has yet to be performed.
 
 - High level client library has yet to be written.
+
+# Higher Level CLI Example
+
+# Lower Level Haskell Example
+
+This example was copy pasted from [the test suite](https://github.com/jpittis/mockit/blob/master/test/ExampleSpec.hs). Remember that the currently exposed API is rather
+simple and low level. Before this project can be used in a real appliaction, a higher level API
+should be build.
+
+```Haskell
+-- Let's start by creating a proxy. It's going to listen on port 4000 and forward to our
+-- example HTTP service on port 5000.
+resp <- sendCommand $ Create "example" "localhost" 4000 "localhost" 5000
+resp `shouldBe` SuccessResp True
+-- We can assert that the proxies properly forwards the HTTP request.
+reqSuccess 4000 `shouldReturn` Success
+
+-- Now we can ensure that disabling the proxy will cause HTTP requests through it to raise
+-- exceptions because there isn't a socket listening on that port.
+resp <- sendCommand $ Update "example" Disabled
+resp `shouldBe` SuccessResp True
+reqSuccess 4000 `shouldReturn` Exception
+
+-- The timeout state won't raise an exception because the's a socket listening. But the
+-- socket isn't accepting connections which means the HTTP request should timeout.
+resp <- sendCommand $ Update "example" Timeout
+resp `shouldBe` SuccessResp True
+reqSuccess 4000 `shouldReturn` TimeoutR
+
+-- Finally, we can move the proxy back into an enabled state and the request should be
+-- successful yet again.
+resp <- sendCommand $ Update "example" Enabled
+resp `shouldBe` SuccessResp True
+reqSuccess 4000 `shouldReturn` Success
+```
--- test/ExampleSpec.hs
+++ test/ExampleSpec.hs
@@ -54,21 +54,37 @@ spec =
         reqSuccess 5000 `shouldReturn` Success
 
     it "attempts all proxy states" $ do
+      -- This is effectively what's run when you execute the mockit-server binary. It's going to
+      -- create an HTTP server listening on port 3000 whose API you will use to create an manipulate
+      -- proxies.
       handler <- startHandler Map.empty handleCommand
       server <- async $ serve handler
+
+      -- We're also going to use startServer to create an example HTTP service on port 5000. It
+      -- always returns success. We're going to be testing a proxy in between this test and this
+      -- example service.
       withAsync startServer $ \_ -> do
+        -- Let's start by creating a proxy. It's going to listen on port 4000 and forward to our
+        -- example HTTP service on port 5000.
         resp <- sendCommand $ Create "example" "localhost" 4000 "localhost" 5000
         resp `shouldBe` SuccessResp True
+        -- We can assert that the proxies properly forwards the HTTP request.
         reqSuccess 4000 `shouldReturn` Success
 
+        -- Now we can ensure that disabling the proxy will cause HTTP requests through it to raise
+        -- exceptions because there isn't a socket listening on that port.
         resp <- sendCommand $ Update "example" Disabled
         resp `shouldBe` SuccessResp True
         reqSuccess 4000 `shouldReturn` Exception
 
+        -- The timeout state won't raise an exception because the's a socket listening. But the
+        -- socket isn't accepting connections which means the HTTP request should timeout.
         resp <- sendCommand $ Update "example" Timeout
         resp `shouldBe` SuccessResp True
         reqSuccess 4000 `shouldReturn` TimeoutR
 
+        -- Finally, we can move the proxy back into an enabled state and the request should be
+        -- successful yet again.
         resp <- sendCommand $ Update "example" Enabled
         resp `shouldBe` SuccessResp True
         reqSuccess 4000 `shouldReturn` Success
